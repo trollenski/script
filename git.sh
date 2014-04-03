ee4dfa06fa7ca2409706d187579d059ae0de7d91
trollenski <root@rastapoupoulos.local.local>
2014-04-03 11:04:06 +0200

    ...

--- git.sh
+++ git.sh
@@ -1,10 +1,18 @@
 # ~/git.sh
 #! /bin/bash
 
+# Initialisation d'un répertoire GITHUB.
 git init
 git add README.txt
-git config --global user.mail "moi_ludovic@hotmail.com"
-git config --global user.name "trollenski"
+
+# Mettre nos paramètres de connection personnel à github.com (adresse mail et
+# login.
+git config --global user.mail "@mail"
+git config --global user.name "login"
+
+# Synchronisation initial avec le GITHUB.
 git commit -m "commit initial"
-git remote add origin https://github.com/trollenski/script.git
+
+# Mettre l'adresse URL de notre répertoire sur github.com
+git remote add origin https://github.com/login/dossier.git
 git push -u origin master
